caf99132f2938ffc045699e1ea3e6ca0ddc77909
Akshita Sanvi <akstansanvi1905@gmail.com>
2025-10-18 09:01:59 +0530

    01-folder done

---
+++ app.js
@@ -0,0 +1,9 @@
+// export const arr = [10,20,30,40]
+// const user='Sarthak'
+// export default user;
+
+const user = 'Sarthak'
+export const age = 89
+export const skills = ['Frontend', 'Backend', 'UI/UX']
+
+export default user
